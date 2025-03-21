d98fe003374676cab5765f7ec6bb23b53a20fca5
Victor Neplyuev <vic4517369@gmail.com>
2025-03-21 07:00:48 +0300

    tryed 9

--- 0_test.js
+++ 0_test.js
@@ -1,14 +1,38 @@
-const data1 = ['AA aa Aa aA'];
-const str = data1[0];
-console.log(str);
-const regex = /[^.\s?!]+/g;
-const found = str.match(regex);
+function leastCommon(arr) {
+  //placing in order
+  arr.sort((a,b)=>a-b);
+  
+  //if two consequtive nums
+  if (arr[1]-arr[0] == 1){
+    let result = arr[0]*arr[1]; 
+    return result;
+  }
 
-console.log(found);
+  //make array of whole numbers 
+  for (let i = arr[0]+1; i < arr[1]; i++){
+    arr.push(i);
+  }
+  arr.sort((a,b)=>a-b);
+  console.log(arr);
 
-const res = new Set();
-found.forEach(element => {
-  res.add(element);
-});
+  //multiple first and last nums for saving a bit of time
+  let result = arr[arr.length-3]*arr[arr.length-2]*arr[arr.length-1];
+  console.log(result);
+  //checking if it`s a  common multiple for each
+  //if not add 1 and check again
+  function check(y, aarr){
+         let chek = aarr.map((x) => y % x);//checking if has reminder after dividing
+         console.log(chek);
+         if (chek.every(function(x){return x == 0}) == false){
+           y++;
+           return check(y, aarr);
+         }
+         else if (chek.every(function(x){return x == 0}) == true){
+           return y;
+         }
+    }  
 
-console.log(res);
+return check(result,arr);
+}
+
+console.log(leastCommon([2,10]));
