9d01ea7641ba76c99fb5e594402389c674124e3d
Victor Neplyuev <vic4517369@gmail.com>
2025-05-01 20:27:14 +0300

    15 in progress

--- 0_test.js
+++ 0_test.js
@@ -1,16 +1,20 @@
-function fName (input) {
-  let dict = new Map();
-  for (let i = 1; 1 <= input.length - 2; i++){
-    let str = input[i].split(" ");
-    dict.set(str[0], str[1]);
+data1 = [ '-530', '-530', '-530', '-2000000000'];
+
+function whatSequence (input) {
+  let diferenceArr = [];
+  let diference;
+
+  for(let i = 1; i <= input.length-2; i++){
+    diference = Number(input[i]) - Number(input[i-1]);
+    diferenceArr.push(diference)
+    
   }
-  return dict;
-};
 
-const data1 = `3
-Hello Hi
-Bye Goodbye
-List Array
-Goodbye`;
+  if(diferenceArr.every((x) => x === 0)){
+    return `CONSTANT`;
+  }
+  else{return 'not'}
 
-console.log(fName(data1));
+}
+
+console.log(whatSequence (data1));
--- task15_codeRun_.js
+++ task15_codeRun_.js
@@ -1,29 +1,42 @@
 /*
 !!Write down algo
+make '-2000000000' as stop marker
 */
 
 function whatSequence (input) {
+  let diferenceArr = [];
+  let diference;
 
+  for(let i = 1; i <= input.length-2; i++){
+    diference = Number(input[i]) - Number(input[i-1]);
+    diferenceArr.push(diference)
+    
+  }
+
+  if(diferenceArr.every((x) => x === 0)){
+    return `CONSTANT`;
+  }
+  else if(diferenceArr.every((x) => x > 0)){}
 };
 
 
 //tests
-const data1 = [ '-530 -530 -530' ];
+const data1 = [ '-530', '-530', '-530', '-2000000000'];
 const expectedOutput1 = `CONSTANT`;
 
-const data2 = [ '1 2 3 4 5 6 7 8 9' ];
+const data2 = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '-2000000000'];
 const expectedOutput2 = `ASCENDING`;
 
-const data3 = [ '-5 13 15 20 30 31' ];
+const data3 = [ '-5', '13', '15', '20', '30', '31', '-2000000000'];
 const expectedOutput3 = `WEAKLY ASCENDING`;
 
-const data4 = [ '7 6 5 4 3 2 1' ];
+const data4 = [ '7', '6', '5', '4', '3', '2', '1', '-2000000000'];
 const expectedOutput4 = `DESCENDING`;
 
-const data5 = [ '31 30 20 15 -1 -17' ];
+const data5 = [ '31', '30', '20', '15', '-1', '-17', '-2000000000'];
 const expectedOutput5 = `WEAKLY DESCENDING`;
 
-const data6 = [ '-14 15 -17 -1 514' ];
+const data6 = [ '-14', '15', '-17', '-1', '514', '-2000000000'];
 const expectedOutput6 = `RANDOM`;
 
 // check
