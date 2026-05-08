3aab8c63b64c0df95290a01f5c68394fac2ec278
Victor Neplyuev <vic4517369@gmail.com>
2026-05-08 07:05:53 +0300

    competition1 - completed

---
+++ YalgoTraining/Block_1/Competition/C2_1.java
@@ -0,0 +1,19 @@
+package YalgoTraining.Block_1.Competition;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class C2_1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        reader.close();
+        writer.close();
+    }
+}
